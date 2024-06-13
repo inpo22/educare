70fe06a20361486bd1c3fa7800ca2655305b3453
LEE_HYEON_WOO <justice3350@naver.com>
2024-06-13 17:02:20 +0900

    로그인 커밋 완료

--- src/main/java/com/edu/care/controller/LoginController.java
+++ src/main/java/com/edu/care/controller/LoginController.java
@@ -5,13 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+
 import com.edu.care.service.LoginService;
 
 @Controller
 public class LoginController {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	LoginService loginService;
 	
-	@Autowired LoginService loginService;
+	/*@GetMapping(value = "/")
+	public String home() {
+		return "login";
+	}*/
 	
 }
