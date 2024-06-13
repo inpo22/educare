1239b33632f3102cab72c3f88ba63177439a6827
hong_in_pyo <inpyo8086@naver.com>
2024-06-13 16:41:40 +0900

    받은 메일함 완료

--- src/main/java/com/edu/care/dao/MailDAO.java
+++ src/main/java/com/edu/care/dao/MailDAO.java
@@ -1,8 +1,20 @@
 package com.edu.care.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.edu.care.dto.MailDTO;
 
 @Mapper
 public interface MailDAO {
+
+	List<MailDTO> receivedMailListCall(int start, int pagePerCnt, String user_code);
+
+	int receivedMailPageCnt(int pagePerCnt, String user_code);
+
+	void mailListRead(String mail_no);
+
+	int mailListDel(String mail_no);
 
 }
