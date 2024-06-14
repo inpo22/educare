cfea00f14d1a85a23c23b9f44a6d408608733aae
LEE_HYEON_WOO <justice3350@naver.com>
2024-06-14 17:00:58 +0900

    로그인 커밋 완료

--- src/main/java/com/edu/care/controller/LoginController.java
+++ src/main/java/com/edu/care/controller/LoginController.java
@@ -1,10 +1,15 @@
 package com.edu.care.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.edu.care.service.LoginService;
 
@@ -16,9 +21,23 @@ public class LoginController {
 	@Autowired
 	LoginService loginService;
 	
-	/*@GetMapping(value = "/")
-	public String home() {
-		return "login";
-	}*/
+	@GetMapping(value = "/login.go")
+	public String login() {
+		logger.info("::최초 로그인 페이지::");
+		return "login/login";
+	}
+	//로그인
+	@PostMapping(value ="/login.do")
+	public String loginAccess(HttpSession session, Model model, String id, String pw,
+			RedirectAttributes redirectAttributes) {
+		logger.info("::최초 로그인 실행::");
+		return loginService.loginAccess(session, model, id, pw, redirectAttributes);
+	}
+	
+	
+	
+	
+	
+	
 	
 }
--- src/main/java/com/edu/care/service/LoginService.java
+++ src/main/java/com/edu/care/service/LoginService.java
@@ -1,19 +1,47 @@
 package com.edu.care.service;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.edu.care.dao.LoginDAO;
+
 
 @Service
 public class LoginService {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
-	
 	@Autowired LoginDAO loginDAO;
+	
+
+	public  String loginAccess(HttpSession session, Model model, String id, String pw, 
+		RedirectAttributes redirectAttributes) {
+		logger.info("id : {} / pw : {}", id, pw);
+		String msg="아이디 또는 비밀번호를 다시 확인하세요";
+		//String page="main/main";
+		//LoginService loginservice = new LoginService();
+		
+		int row = loginDAO.loginAccess(id,pw);
+		if(row >0) {
+			session.setAttribute("loginId", id);
+			model.addAttribute("loginId", id);
+			model.addAttribute("loginSuccess", id+ "로그인 성공");
+			logger.info("id2 : {} / pw2 : {}", id, pw);
+			return "redirect:/";
+		}else {
+			redirectAttributes.addFlashAttribute("loginError", true);
+			return "redirect:/login.go";
+			
+		}
+	}
+
+
+
 
 	
 
-	
-	
 }
