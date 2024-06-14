701997ede7d07c603d3466080206651c586d4714
HEO <sseungg95@gmail.com>
2024-06-14 17:00:59 +0900

    일정 등록 / DB데이터 캘린더에 출력

--- src/main/java/com/edu/care/service/ScheduleService.java
+++ src/main/java/com/edu/care/service/ScheduleService.java
@@ -1,11 +1,16 @@
 package com.edu.care.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edu.care.dao.ScheduleDAO;
+import com.edu.care.dto.ScheduleDTO;
 
 @Service
 public class ScheduleService {
@@ -13,5 +18,16 @@ public class ScheduleService {
 	Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@Autowired ScheduleDAO scheduleDAO;
+
+	public int scheduleWrite(ScheduleDTO scheduleDTO) {
+		return scheduleDAO.scheduleWrite(scheduleDTO);
+	}
+
+	public Map<String, Object> scheduleList() {
+		Map<String, Object> result = new HashMap<String, Object>();
+		List<ScheduleDTO> list = scheduleDAO.scheduleList();
+		result.put("list", list);
+		return result;
+	}
 	
 }
